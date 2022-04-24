89aeba8a43c0b3f6485abe5a82872b7e7e8e0cd6
athiyadeviyani <adeviyani@cmu.edu>
2022-04-24 19:03:03 -0400

    hopefully fixed

--- model/edsr.py
+++ model/edsr.py
@@ -1,6 +1,6 @@
 from tensorflow.python.keras.layers import Add, Conv2D, Input, Lambda
 from tensorflow.python.keras.models import Model
-from model.attention import Conv2D_attention
+from model.attention import Conv2D_attention, ChannelAttention, SpatialAttention
 
 from model.common import normalize, denormalize, pixel_shuffle
 
@@ -9,14 +9,21 @@ def edsr(scale, num_filters=64, num_res_blocks=8, res_block_scaling=None, attent
     x = Lambda(normalize)(x_in)
 
     if attention:
-        x = b = Conv2D_attention(x, numFilters=num_filters, kernelSize=3)
+        x = b = Conv2D(num_filters, 3, padding='same')(x)
+        x = b = ChannelAttention(num_filters, 3)(x)
+        x = b = SpatialAttention(8)(x)
     else:
         x = b = Conv2D(num_filters, 3, padding='same')(x)
+    
     for i in range(num_res_blocks):
         b = res_block(b, num_filters, res_block_scaling, attention=attention)
 
     if attention:
-        b = Conv2D_attention(b, numFilters=num_filters, kernelSize=3)
+        # b = Conv2D_attention(b, numFilters=num_filters, kernelSize=3)
+        b = Conv2D(num_filters, 3, padding='same')(b)
+        b = ChannelAttention(num_filters, 3)(b)
+        b = SpatialAttention(8)(b)
+# 
     else:
         b = Conv2D(num_filters, 3, padding='same')(b)
     x = Add()([x, b])
@@ -24,7 +31,10 @@ def edsr(scale, num_filters=64, num_res_blocks=8, res_block_scaling=None, attent
     x = upsample(x, scale, num_filters)
     
     if attention:
-        x = Conv2D_attention(x, numFilters=3, kernelSize=3)
+        # x = Conv2D_attention(x, numFilters=3, kernelSize=3)
+        x = Conv2D(3, 3, padding='same')(x)
+        x = ChannelAttention(3, 3)(x)
+        x = SpatialAttention(8)(x)
     else:
         x = Conv2D(3, 3, padding='same')(x)
 
@@ -34,12 +44,18 @@ def edsr(scale, num_filters=64, num_res_blocks=8, res_block_scaling=None, attent
 
 def res_block(x_in, filters, scaling, attention=False):
     if attention:
-        x = Conv2D_attention(x_in, numFilters=filters, kernelSize=3, activation='relu')
+        # x = Conv2D_attention(x_in, numFilters=filters, kernelSize=3, activation='relu')
+        x = Conv2D(filters, 3, padding='same', activation='relu')(x_in)
+        x = ChannelAttention(filters, 3)(x)
+        x = SpatialAttention(8)(x)
     else:
         x = Conv2D(filters, 3, padding='same', activation='relu')(x_in)
     
     if attention:
-        x = Conv2D_attention(x, numFilters=filters, kernelSize=3)
+        # x = Conv2D_attention(x, numFilters=filters, kernelSize=3)
+        x = Conv2D(filters, 3, padding='same', activation='relu')(x)
+        x = ChannelAttention(filters, 3)(x)
+        x = SpatialAttention(8)(x)
     else:
         x = Conv2D(filters, 3, padding='same')(x)
     if scaling:
@@ -51,7 +67,10 @@ def res_block(x_in, filters, scaling, attention=False):
 def upsample(x, scale, num_filters, attention=False):
     def upsample_1(x, factor, attention=False, **kwargs):
         if attention:
-            x = Conv2D_attention(x, numFilters=num_filters * (factor ** 2), kernelSize=3)
+            # x = Conv2D_attention(x, numFilters=num_filters * (factor ** 2), kernelSize=3)
+            x = Conv2D(num_filters * (factor ** 2), 3, padding='same', **kwargs)(x)
+            x = ChannelAttention(num_filters * (factor ** 2), 3)(x)
+            x = SpatialAttention(8)(x)
         else:
             x = Conv2D(num_filters * (factor ** 2), 3, padding='same', **kwargs)(x)
         return Lambda(pixel_shuffle(scale=factor))(x)
--- model/srgan.py
+++ model/srgan.py
@@ -1,7 +1,7 @@
 from tensorflow.keras.layers import Add, BatchNormalization, Conv2D, Dense, Flatten, Input, LeakyReLU, PReLU, Lambda
 from tensorflow.keras.models import Model
 from tensorflow.keras.applications.vgg19 import VGG19
-from model.attention import Conv2D_attention
+from model.attention import Conv2D_attention, ChannelAttention, SpatialAttention
 
 from model.common import pixel_shuffle, normalize_01, normalize_m11, denormalize_m11
 
@@ -13,7 +13,10 @@ def upsample(x_in, num_filters, attention=False):
     if not attention:
         x = Conv2D(num_filters, kernel_size=3, padding='same')(x_in)
     else:
-        x = Conv2D_attention(x_in, numFilters=num_filters, kernelSize=3)
+        # x = Conv2D_attention(x_in, numFilters=num_filters, kernelSize=3)
+        x = Conv2D(num_filters, kernel_size=3, padding='same')(x_in)
+        x = ChannelAttention(num_filters, 3)(x)
+        x = SpatialAttention(8)(x)
     x = Lambda(pixel_shuffle(scale=2))(x)
     return PReLU(shared_axes=[1, 2])(x)
 
@@ -22,14 +25,20 @@ def res_block(x_in, num_filters, momentum=0.8, attention=False):
     if not attention:
         x = Conv2D(num_filters, kernel_size=3, padding='same')(x_in)
     else:
-        x = Conv2D_attention(x_in, numFilters=num_filters, kernelSize=3)
+        # x = Conv2D_attention(x_in, numFilters=num_filters, kernelSize=3)
+        x = Conv2D(num_filters, kernel_size=3, padding='same')(x_in)
+        x = ChannelAttention(num_filters, 3)(x)
+        x = SpatialAttention(8)(x)
     x = BatchNormalization(momentum=momentum)(x)
     x = PReLU(shared_axes=[1, 2])(x)
     
     if not attention:
         x = Conv2D(num_filters, kernel_size=3, padding='same')(x)
     else:
-        x = Conv2D_attention(x, numFilters=num_filters, kernelSize=3)
+        # x = Conv2D_attention(x, numFilters=num_filters, kernelSize=3)
+        x = Conv2D(num_filters, kernel_size=3, padding='same')(x)
+        x = ChannelAttention(num_filters, 3)(x)
+        x = SpatialAttention(8)(x)
     x = BatchNormalization(momentum=momentum)(x)
     x = Add()([x_in, x])
     return x
@@ -42,7 +51,10 @@ def sr_resnet(num_filters=64, num_res_blocks=16, attention=False):
     if not attention:
         x = Conv2D(num_filters, kernel_size=9, padding='same')(x)
     else:
-        x = Conv2D_attention(x, numFilters=num_filters, kernelSize=9)
+        # x = Conv2D_attention(x, numFilters=num_filters, kernelSize=9)
+        x = Conv2D(num_filters, kernel_size=9, padding='same')(x)
+        x = ChannelAttention(num_filters, 9)(x)
+        x = SpatialAttention(8)(x)
     x = x_1 = PReLU(shared_axes=[1, 2])(x)
 
     for _ in range(num_res_blocks):
@@ -51,7 +63,10 @@ def sr_resnet(num_filters=64, num_res_blocks=16, attention=False):
     if not attention:
         x = Conv2D(num_filters, kernel_size=3, padding='same')(x)
     else:
-        x = Conv2D_attention(x, numFilters=num_filters, kernelSize=3)
+        # x = Conv2D_attention(x, numFilters=num_filters, kernelSize=3)
+        x = Conv2D(num_filters, kernel_size=3, padding='same')(x)
+        x = ChannelAttention(num_filters, 3)(x)
+        x = SpatialAttention(8)(x)
     x = BatchNormalization()(x)
     x = Add()([x_1, x])
 
@@ -61,7 +76,10 @@ def sr_resnet(num_filters=64, num_res_blocks=16, attention=False):
     if not attention:
         x = Conv2D(3, kernel_size=9, padding='same', activation='tanh')(x)
     else:
-        x = Conv2D_attention(x, numFilters=num_filters, kernelSize=3, activation='tanh')
+        # x = Conv2D_attention(x, numFilters=num_filters, kernelSize=3, activation='tanh')
+        x = Conv2D(3, kernel_size=9, padding='same', activation='tanh')(x)
+        x = ChannelAttention(3, 9)(x)
+        x = SpatialAttention(8)(x)
     x = Lambda(denormalize_m11)(x)
 
     return Model(x_in, x)
@@ -74,7 +92,10 @@ def discriminator_block(x_in, num_filters, strides=1, batchnorm=True, momentum=0
     if not attention:
         x = Conv2D(num_filters, kernel_size=3, strides=strides, padding='same')(x_in)
     else:
-        x = Conv2D_attention(x_in, numFilters=num_filters, kernelSize=3, strides=strides)
+        # x = Conv2D_attention(x_in, numFilters=num_filters, kernelSize=3, strides=strides)
+        x = Conv2D(num_filters, kernel_size=3, strides=strides, padding='same')(x_in)
+        x = ChannelAttention(num_filters, 3)(x)
+        x = SpatialAttention(8)(x)
     if batchnorm:
         x = BatchNormalization(momentum=momentum)(x)
     return LeakyReLU(alpha=0.2)(x)
