ba17de930cc6a3821ff9eae914172725e67f42a0
athiyadeviyani <adeviyani@cmu.edu>
2022-04-25 21:30:44 -0400

    add run WDSR

--- model/wdsr.py
+++ model/wdsr.py
@@ -2,6 +2,8 @@ import tensorflow_addons as tfa
 
 from tensorflow.python.keras.layers import Add, Conv2D, Input, Lambda
 from tensorflow.python.keras.models import Model
+from model.attention import ChannelAttention, SpatialAttention
+
 
 from model.common import normalize, denormalize, pixel_shuffle
 
@@ -14,19 +16,36 @@ def wdsr_b(scale, num_filters=32, num_res_blocks=8, res_block_expansion=6, res_b
     return wdsr(scale, num_filters, num_res_blocks, res_block_expansion, res_block_scaling, res_block_b)
 
 
-def wdsr(scale, num_filters, num_res_blocks, res_block_expansion, res_block_scaling, res_block):
+def wdsr(scale, num_filters, num_res_blocks, res_block_expansion, res_block_scaling, res_block, attention=False):
     x_in = Input(shape=(None, None, 3))
     x = Lambda(normalize)(x_in)
 
     # main branch
-    m = conv2d_weightnorm(num_filters, 3, padding='same')(x)
+    if attention:
+        m = conv2d_weightnorm(num_filters, 3, padding='same')(x)
+        m = ChannelAttention(num_filters, 3)(m)
+        m = SpatialAttention(8)(m)
+    else:
+        m = conv2d_weightnorm(num_filters, 3, padding='same')(x)
+
     for i in range(num_res_blocks):
-        m = res_block(m, num_filters, res_block_expansion, kernel_size=3, scaling=res_block_scaling)
-    m = conv2d_weightnorm(3 * scale ** 2, 3, padding='same', name=f'conv2d_main_scale_{scale}')(m)
+        m = res_block(m, num_filters, res_block_expansion, kernel_size=3, scaling=res_block_scaling, attention=attention)
+
+    if attention:
+        m = conv2d_weightnorm(3 * scale ** 2, 3, padding='same', name=f'conv2d_main_scale_{scale}')(m)
+        m = ChannelAttention(3 * scale ** 2, 3)(m)
+        m = SpatialAttention(8)(m)
+    else:
+        m = conv2d_weightnorm(3 * scale ** 2, 3, padding='same', name=f'conv2d_main_scale_{scale}')(m)
     m = Lambda(pixel_shuffle(scale))(m)
 
     # skip branch
-    s = conv2d_weightnorm(3 * scale ** 2, 5, padding='same', name=f'conv2d_skip_scale_{scale}')(x)
+    if attention:
+        s = conv2d_weightnorm(3 * scale ** 2, 5, padding='same', name=f'conv2d_skip_scale_{scale}')(x)
+        s = ChannelAttention(3 * scale ** 2, 3)(s)
+        s = SpatialAttention(8)(s)
+    else:   
+        s = conv2d_weightnorm(3 * scale ** 2, 5, padding='same', name=f'conv2d_skip_scale_{scale}')(x)
     s = Lambda(pixel_shuffle(scale))(s)
 
     x = Add()([m, s])
@@ -35,20 +54,26 @@ def wdsr(scale, num_filters, num_res_blocks, res_block_expansion, res_block_scal
     return Model(x_in, x, name="wdsr")
 
 
-def res_block_a(x_in, num_filters, expansion, kernel_size, scaling):
+def res_block_a(x_in, num_filters, expansion, kernel_size, scaling, attention=False):
     x = conv2d_weightnorm(num_filters * expansion, kernel_size, padding='same', activation='relu')(x_in)
     x = conv2d_weightnorm(num_filters, kernel_size, padding='same')(x)
+    if attention:
+        x = ChannelAttention(num_filters, 3)(x)
+        x = SpatialAttention(8)(x)
     if scaling:
         x = Lambda(lambda t: t * scaling)(x)
     x = Add()([x_in, x])
     return x
 
 
-def res_block_b(x_in, num_filters, expansion, kernel_size, scaling):
+def res_block_b(x_in, num_filters, expansion, kernel_size, scaling, attention=False):
     linear = 0.8
     x = conv2d_weightnorm(num_filters * expansion, 1, padding='same', activation='relu')(x_in)
     x = conv2d_weightnorm(int(num_filters * linear), 1, padding='same')(x)
     x = conv2d_weightnorm(num_filters, kernel_size, padding='same')(x)
+    if attention:
+        x = ChannelAttention(num_filters, 3)(x)
+        x = SpatialAttention(8)(x)
     if scaling:
         x = Lambda(lambda t: t * scaling)(x)
     x = Add()([x_in, x])
